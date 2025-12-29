b1a9fede33b8d326f9b7bb913fe32a2c3c48da96
Paweł Gajda <115713041+pawgajda@users.noreply.github.com>
2025-12-29 16:11:06 +0100

    chore(go): bit_counting.go update

--- go/bit_counting.go
+++ go/bit_counting.go
@@ -1,4 +1,6 @@
 // https://www.codewars.com/kata/526571aae218b8ee490006f4
+//
+// attempt #1:
 package kata
 
 import (
@@ -9,4 +11,13 @@ import (
 func CountBits(n uint) int {
   bin := strconv.FormatInt(int64(n), 2)
   return strings.Count(bin, "1")
+}
+//
+// attempt #2:
+package kata
+
+import "math/bits"
+
+func CountBits(n uint) int {
+    return bits.OnesCount(n)
 }
